3312e6e9113b07f3e2fb7f6dba2ef387d977f758
Priyank Solanki <psolanki@condorcet93.fr>
2025-10-14 12:07:23 +0200

    add details page of apprentice student

--- src/main/java/altn72/tp_fil_rouge/controllers/DashBoardController.java
+++ src/main/java/altn72/tp_fil_rouge/controllers/DashBoardController.java
@@ -3,13 +3,17 @@ package altn72.tp_fil_rouge.controllers;
 import altn72.tp_fil_rouge.entities.AnneeAcademique;
 import altn72.tp_fil_rouge.entities.Apprenti;
 import altn72.tp_fil_rouge.entities.TuteurEnseignant;
+import altn72.tp_fil_rouge.exceptions.ApprentiNotFoundException;
 import altn72.tp_fil_rouge.exceptions.TuteurEnseignantNotFoundException;
 import altn72.tp_fil_rouge.services.AnneeAcademiqueService;
+import altn72.tp_fil_rouge.services.ApprentiService;
 import altn72.tp_fil_rouge.services.TuteurEnseignantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 
 @Controller
@@ -19,6 +23,8 @@ public class DashBoardController {
     private AnneeAcademiqueService  anneeAcademiqueService;
     @Autowired
     private TuteurEnseignantService tuteurEnseignantService;
+    @Autowired
+    private ApprentiService apprentiService;
 
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
@@ -46,5 +52,12 @@ public class DashBoardController {
         return "Dashboard";
     }
 
-
+    @GetMapping("/apprentice/{id}")
+    public String afficherInfosApprenti(@PathVariable Integer id, Model model) {
+        Apprenti apprenti = apprentiService.getById(id).orElseThrow(
+                () -> new ApprentiNotFoundException(id)
+        );
+        model.addAttribute("apprenti", apprenti);
+        return "ApprentiInfos";
+    }
 }
--- src/main/java/altn72/tp_fil_rouge/services/ApprentiService.java
+++ src/main/java/altn72/tp_fil_rouge/services/ApprentiService.java
@@ -1,11 +1,13 @@
 package altn72.tp_fil_rouge.services;
 
 import altn72.tp_fil_rouge.entities.Apprenti;
+import altn72.tp_fil_rouge.entities.TuteurEnseignant;
 import altn72.tp_fil_rouge.repositories.ApprentiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ApprentiService {
@@ -15,5 +17,15 @@ public class ApprentiService {
 
     public List<Apprenti> getAll() {
         return apprentiRepository.findAll();
+    }
+
+    public Optional<Apprenti> getById(Integer id) {
+        Apprenti apprenti = apprentiRepository.findById(Long.valueOf(id)).orElse(null);
+
+        if(apprenti == null) {
+            return Optional.empty();
+        }else{
+            return Optional.of(apprenti);
+        }
     }
 }
