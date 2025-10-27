8d4de361979d35cd6e92f64d29e773cab79619e2
Priyank Solanki <psolanki@condorcet93.fr>
2025-10-27 19:42:56 +0100

    query for function search in ApprentiRepository

--- src/main/java/altn72/tp_fil_rouge/repositories/ApprentiRepository.java
+++ src/main/java/altn72/tp_fil_rouge/repositories/ApprentiRepository.java
@@ -10,12 +10,6 @@ import java.util.List;
 import java.util.List;
 
 public interface ApprentiRepository extends JpaRepository<Apprenti, Long> {
-    @Query("""
-    SELECT a FROM Apprenti a WHERE 
-      (LOWER(a.prenom) LIKE LOWER(CONCAT('%',:name, '%')) OR LOWER(a.nom) LIKE LOWER(CONCAT('%',:name,'%'))) AND
-      (LOWER(a.mission.motsCles) LIKE LOWER(CONCAT('%',:keywords,'%')) )AND
-      (LOWER(a.entreprise.raisonSociale) LIKE LOWER(CONCAT('%',:enterprise, '%'))) AND
-      (LOWER(a.anneeAcademique.annee) LIKE LOWER(CONCAT('%',:promotion, '%')))""")
 
     // Apprentis qui n'ont PAS de maître d'apprentissage
     List<Apprenti> findByMaitreApprentissageIsNull();
@@ -23,6 +17,13 @@ public interface ApprentiRepository extends JpaRepository<Apprenti, Long> {
     // (optionnel) version qui charge aussi entreprise et tuteur pour éviter le N+1 dans Thymeleaf
     @EntityGraph(attributePaths = {"entreprise", "tuteurEnseignant"})
     List<Apprenti> findAllByMaitreApprentissageIsNull();
+
+    @Query("""
+    SELECT a FROM Apprenti a WHERE 
+      (LOWER(a.prenom) LIKE LOWER(CONCAT('%',:name, '%')) OR LOWER(a.nom) LIKE LOWER(CONCAT('%',:name,'%'))) AND
+      (LOWER(a.mission.motsCles) LIKE LOWER(CONCAT('%',:keywords,'%')) )AND
+      (LOWER(a.entreprise.raisonSociale) LIKE LOWER(CONCAT('%',:enterprise, '%'))) AND
+      (LOWER(a.anneeAcademique.annee) LIKE LOWER(CONCAT('%',:promotion, '%')))""")
     List<Apprenti> search(
             @Param("name") String name,
             @Param("enterprise") String enterprise,
