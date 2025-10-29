2cc3cdc77e1259991df5ecda16d2720be8597a78
Priyank Solanki <psolanki@condorcet93.fr>
2025-10-29 13:21:31 +0100

    fix display info apprentie when null

--- src/main/java/altn72/projet/services/ApprentiService.java
+++ src/main/java/altn72/projet/services/ApprentiService.java
@@ -4,8 +4,6 @@ import altn72.projet.entities.Apprenti;
 import altn72.projet.repositories.ApprentiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,10 +12,6 @@ public class ApprentiService {
 
     @Autowired
     private ApprentiRepository apprentiRepository;
-
-    public List<Apprenti> getAll() {
-        return apprentiRepository.findAll();
-    }
 
     public Optional<Apprenti> getById(Integer id) {
         Apprenti apprenti = apprentiRepository.findById(Long.valueOf(id)).orElse(null);
@@ -32,7 +26,7 @@ public class ApprentiService {
     public List<Apprenti> search(String name, String enterprise, String promotion,String keywords) {
 
         if(name == null || enterprise == null || promotion == null || keywords == null || name.isEmpty() && enterprise.isEmpty() && promotion.isEmpty() && keywords.isEmpty()) {
-            return new ArrayList<>();
+            return apprentiRepository.findAll();
         }
         return apprentiRepository.search(name, enterprise, promotion, keywords);
     }
